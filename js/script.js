518f20812dd7ef6f8a7e2a799dc1a9abb3cbe238
Nathan Yi <nathanyi112@gmail.com>
2022-03-26 21:42:28 -0500

    adds css

--- js/script.js
+++ js/script.js
@@ -7,32 +7,47 @@
 
 const $input = $('input')
 const $button = $('button')
+const $dayInput = $('#days').val()
+console.log($dayInput)
 
 
 $button.on("click", () => {
     const cityName = $input.val();
 
 $.ajax(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f738070ba746c43b56c8ce529808d062`).then((data) => {
-    console.log(data)
-    const cityLat = data.coord.lat
-    const cityLong = data.coord.lon
-    const cityName = data.name
-    console.log(cityLat)
-    console.log(cityLong)
-    $.ajax(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLong}&exclude=hourly,daily&units=imperial&appid=f738070ba746c43b56c8ce529808d062`
-    ).then((data) => {
-      console.log(data);
-      $("#city").html(`
+  console.log(data);
+  const cityLat = data.coord.lat;
+  const cityLong = data.coord.lon;
+  const cityName = data.name;
+  console.log(cityLat);
+  console.log(cityLong);
+  $.ajax(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLong}&exclude=hourly&units=imperial&appid=f738070ba746c43b56c8ce529808d062`
+  ).then((data) => {
+    console.log(data);
+    $("#city").html(`
           ${cityName}`);
-      $(`#temp`).html(`${data.current.temp}F`);
-      $("#feels").html(`
-      ${data.current.feels_like}`)
-      $("#weather").html(`
-      ${data.current.weather[0].main}`)
-    });
+    $(`#temp`).html(`${data.current.temp}F`);
+    $("#feels").html(`
+      ${data.current.feels_like}F`);
+    $("#weather").html(`
+      ${data.current.weather[0].main}`);
+  });
+  console.log($dayInput);
 
-  
+  // console.log($dayInput.val());
+  //       console.log(data.daily[0].temp)
+
+  // use this to call on multiple days (changed "daily" in url to "minutely")
+  // $.ajax(`
+  // https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLong}&exclude=hourly,minutely&units=imperial&appid=f738070ba746c43b56c8ce529808d062`).then((data) => {
+  //   console.log(data);
+  //   if ($($dayInput.val()) === "one") {
+  //     $("#temp-two").html(`
+  //       ${data.current.temp}`)
+  //       console.log(data.daily[0].temp)
+  //   }
+  // })
 })
 })
 
